80963ff244b5fc52233c4db5186ef00f6d8b4310
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-08-02 13:57:07 -0400

    Created async funtions in app.js and DisplayMusic funtion in DisplayMusic.jsx.

--- music-library-fe/src/App.js
+++ music-library-fe/src/App.js
@@ -1,10 +1,39 @@
 
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+  
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data);
+  }
+
+  async function createSong(newSong){
+    let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong);
+    if(response.status === 201){
+      await getAllSongs();
+    }
+  } 
+
+  async function deleteSong(song){
+    let response = await axios.delete('http://127.0.0.1:8000/api/music/', song);
+    if(response.status === 200){
+      await getAllSongs();
+    }
+  }
   return (
-    <div className="App">
-      <p>hello world</p>
+    <div className='page-container'>
+      <div><DisplayMusic displaySongs = {songs} deleteSongProp = {deleteSong}/></div> 
     </div>
   );
 }
---
+++ music-library-fe/src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+
+const DisplayMusic = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    return (  
+    <div className = 'display-container'>
+        <div className='table-title'>
+            <div className ='library-contain'>
+                <h2 className='library-title'>Song List</h2>
+            </div>
+            <div className = 'search-filter'>
+                <label className='search-label'>Filter Song List:</label>
+                <input type='text' className='custom-input' placeholder="Search...(e.g. Pop music)" onChange={(event) => setSearchTerm(event.target.value)}/>
+            </div>
+        </div>
+        <table className='song-table table'>
+        <thead>
+            <tr className = 'header-row'>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>            
+                <th>Release Date</th>
+                <th>Genre</th>
+                <th>Likes</th>
+            </tr>
+        </thead>
+        <tbody>
+            {props.displaySongs.filter((song) =>{
+                if (searchTerm == ""){
+                    return song;
+                }
+                else if (song.title.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.artist.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.album.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.release_date.toLowerCase().includes(searchTerm.toLocaleLowerCase()) || song.genre.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                    return song;
+                }
+            })
+            .map((song, index) => { 
+            return (
+                <tr key= {index} className = 'display-rows'>
+                    <td>{song.title}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.album}</td>
+                    <td>{song.release_date}</td>
+                    <td>{song.genre}</td>
+                    <td className ='dbutton-contain'><button type = 'submit' className ='delete-button' onClick={() => props.deleteSongProp(song)}>Delete</button></td>
+                </tr>
+            )
+            })}
+        </tbody>
+        </table>
+    </div>     
+    );
+}
+ 
+export default DisplayMusic;
